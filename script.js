c7d958687d3aa15a004eb843aa417f4cc44d8c28
Jan Boťa <botajan@fit.cvut.cz>
2025-03-24 15:53:58 +0100

    Add page about me.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const numStars = 100;
-    const body = document.body;
+    const starsContainer = document.querySelector(".stars");
     for (let i = 0; i < numStars; i++) {
         let star = document.createElement("div");
         star.className = "star";
         star.style.top = Math.random() * 100 + "vh";
         star.style.left = Math.random() * 100 + "vw";
         star.style.animationDuration = (Math.random() * 2 + 1) + "s";
-        body.appendChild(star);
+        starsContainer.appendChild(star);
     }
+});
+
+document.getElementById("showMore").addEventListener("click", () => {
+    window.location.href = "about.html";
 });
